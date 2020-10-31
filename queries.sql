133880066d03c583dbd8cff669d7e6fc62f3ed4f
Corinne Seckinger <cseckinger0525@gmail.com>
2020-10-31 17:00:50 -0500

    finished part 2

--- queries.sql
+++ queries.sql
@@ -1,6 +1,13 @@
 ## Part 1: Test it with SQL
+Columns:
+id int(11) PK
+employer varchar(255)
+name varchar(255)
+skills varchar(255)
 
 ## Part 2: Test it with SQL
+
+SELECT name FROM techjobs.employer WHERE location = "St. Louis City"
 
 ## Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,22 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull (message = "Location required.")
+    private String location;
 
+    public Employer (){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
